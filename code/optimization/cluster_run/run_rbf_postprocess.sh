263285791fe47bf9a7feed3f46107fa5bc6be6bd
Andrew Hamilton <ahamilton144@gmail.com>
2020-12-02 23:10:24 +0000

    update 2obj retest

--- code/optimization/cluster_run/run_rbf_postprocess.sh
+++ code/optimization/cluster_run/run_rbf_postprocess.sh
@@ -5,9 +5,9 @@ nseeds=10
 dir=../../../data/optimization_output/${nobj}obj_rbf_overall
 mkdir $dir
 
-#for nrbf in 1 2 3 4 8 12 
-#do
-#	sh rbf_postprocess_1.sh $nobj $nrbf $nseeds
-#done
+for nrbf in 1 2 3 4 8 12 
+do
+	sh rbf_postprocess_1.sh $nobj $nrbf $nseeds
+done
 
 sh rbf_postprocess_2.sh $nobj $nseeds $dir
