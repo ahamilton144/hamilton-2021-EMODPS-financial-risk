9392a08c4fccd3245b835131ed4f7ee53eff2633
Andrew Hamilton <ahamilton144@users.noreply.github.com>
2021-01-06 10:47:51 -0500

    Update rbf_postprocess_2.sh

--- code/optimization/cluster_run/rbf_postprocess_2.sh
+++ code/optimization/cluster_run/rbf_postprocess_2.sh
@@ -6,9 +6,11 @@ dir=$3
 
 formulation=${nobj}obj_rbf_overall
 ref_fil=${dir}/DPS_${formulation}_borg.reference
+hv_fil=${dir}/DPS_${formulation}_borg.hypervolume
 
-# find overall reference file including all seeds from all rbf trials
+# find overall reference file & hypervolume including all seeds from all rbf trials
 sh find_refSets_combined.sh $nobj $formulation
+sh find_hypervolume.sh $ref_fil $hv_fil
 
 # find performance of each seed relative to reference file
 for nrbf in 1 2 3 4 8 12
